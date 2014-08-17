95223a6015ba8f9e8dc49cf1ae2e976106481705
Vinay <vinay.bhargav.reddy@gmail.com>
2014-08-17 11:40:17 +0530

    minor changes to question screen

--- src/com/amcolabs/quizapp/screens/QuestionScreen.java
+++ src/com/amcolabs/quizapp/screens/QuestionScreen.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.widget.TableLayout;
-import android.widget.TableRow;
 
 import com.amcolabs.quizapp.AppController;
 import com.amcolabs.quizapp.Screen;
@@ -16,13 +15,37 @@ public class QuestionScreen extends Screen {
 	private ArrayList<String> options;
 	private Bitmap image;
 	
+	
+	
 	private TableLayout mainTableView;
 	
 	public QuestionScreen(AppController controller) {
 		super(controller);
+		Context tmp = controller.getContext();
 	}
 
-	public void loadQuestion(){
+	public void loadQuestion(String ques, ArrayList<String> opt, Bitmap img){
+		question = ques;
+		options = opt;
+		image = img;
+	}
+	
+	public void resetProgress(){
+		
+	}
+	
+	public void showNextQuestion(){
+		animateQuestionTextChange();
+		animateQuestionOptionsChange();
+	}
+
+	private void animateQuestionOptionsChange() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void animateQuestionTextChange() {
+		// TODO Auto-generated method stub
 		
 	}
 }
