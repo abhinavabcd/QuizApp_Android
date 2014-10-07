978233ab9026a524fbfec9e8a004ae60a52efa03
abhinav2 <asolipuram@kiwiup.com>
2014-10-07 17:22:43 +0530

    fade in music effect added

--- src/com/amcolabs/quizapp/MusicService.java
+++ src/com/amcolabs/quizapp/MusicService.java
@@ -1,5 +1,8 @@
 package com.amcolabs.quizapp;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -8,8 +11,6 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.widget.MediaController;
 import android.widget.Toast;
-
-import com.amcolabs.quizapp.R;
 
 //---------------------------------------------------------------------------------------
 public class MusicService extends Service 
@@ -28,7 +29,7 @@ public class MusicService extends Service
     
     // Instance of media player
     MediaPlayer mPlayer;
-    
+    private static int FADE_IN_DURATION = 3000;
     // Saves the data buffer of the media player (used by MediaController).
     private int mBuffer = 0;
 
@@ -97,6 +98,8 @@ public class MusicService extends Service
 	
 	public void create(){
     	mPlayer = MediaPlayer.create(this, musicId);
+    	mPlayer.setLooping(true);
+
     	mPlayer.setOnErrorListener(this);
 
     	mPlayer.setOnErrorListener(new OnErrorListener() {
@@ -160,7 +163,7 @@ public class MusicService extends Service
 		if(isPlaying() == false)
 		{
 			seekTo(getCurrentPosition());
-			start();
+			play(FADE_IN_DURATION);
 		}
 	}
 	
@@ -198,9 +201,116 @@ public class MusicService extends Service
 			@Override
 			public void run()
 			{
-				mPlayer.start();
+				play(FADE_IN_DURATION);
 			}
 		});
 		th.start();
 	}
+	
+	private int iVolume;
+
+	private final static int INT_VOLUME_MAX = 100;
+	private final static int INT_VOLUME_MIN = 0;
+	private final static float FLOAT_VOLUME_MAX = 1;
+	private final static float FLOAT_VOLUME_MIN = 0;
+	
+	public void play(int fadeDuration){
+	    //Set current volume, depending on fade or not
+	    if (fadeDuration > 0) 
+	        iVolume = INT_VOLUME_MIN;
+	    else 
+	        iVolume = INT_VOLUME_MAX;
+
+	    updateVolume(0);
+
+	    //Play music
+	    if(!mPlayer.isPlaying()) play(FADE_IN_DURATION);
+
+	    //Start increasing volume in increments
+	    if(fadeDuration > 0)
+	    {
+	        final Timer timer = new Timer(true);
+	        TimerTask timerTask = new TimerTask() 
+	        {
+	            @Override
+	            public void run() 
+	            {
+	                updateVolume(1);
+	                if (iVolume == INT_VOLUME_MAX)
+	                {
+	                    timer.cancel();
+	                    timer.purge();
+	                }
+	            }
+	        };
+
+	        // calculate delay, cannot be zero, set to 1 if zero
+	        int delay = fadeDuration/INT_VOLUME_MAX;
+	        if (delay == 0) delay = 1;
+
+	        timer.schedule(timerTask, delay, delay);
+	    }
+	}
+
+	public void pause(int fadeDuration)
+	{
+	    //Set current volume, depending on fade or not
+	    if (fadeDuration > 0) 
+	        iVolume = INT_VOLUME_MAX;
+	    else 
+	        iVolume = INT_VOLUME_MIN;
+
+	    updateVolume(0);
+
+	    //Start increasing volume in increments
+	    if(fadeDuration > 0)
+	    {
+	        final Timer timer = new Timer(true);
+	        TimerTask timerTask = new TimerTask() 
+	        {
+	            @Override
+	            public void run() 
+	            {   
+	                updateVolume(-1);
+	                if (iVolume == INT_VOLUME_MIN)
+	                {
+	                    //Pause music
+	                    if (mPlayer.isPlaying()) mPlayer.pause();
+	                    timer.cancel();
+	                    timer.purge();
+	                }
+	            }
+	        };
+
+	        // calculate delay, cannot be zero, set to 1 if zero
+	        int delay = fadeDuration/INT_VOLUME_MAX;
+	        if (delay == 0) delay = 1;
+
+	        timer.schedule(timerTask, delay, delay);
+	    }           
+	}
+
+	private void updateVolume(int change)
+	{
+	    //increment or decrement depending on type of fade
+	    iVolume = iVolume + change;
+
+	    //ensure iVolume within boundaries
+	    if (iVolume < INT_VOLUME_MIN)
+	        iVolume = INT_VOLUME_MIN;
+	    else if (iVolume > INT_VOLUME_MAX)
+	        iVolume = INT_VOLUME_MAX;
+
+	    //convert to float value
+	    float fVolume = 1 - ((float) Math.log(INT_VOLUME_MAX - iVolume) / (float) Math.log(INT_VOLUME_MAX));
+
+	    //ensure fVolume within boundaries
+	    if (fVolume < FLOAT_VOLUME_MIN)
+	        fVolume = FLOAT_VOLUME_MIN;
+	    else if (fVolume > FLOAT_VOLUME_MAX)
+	        fVolume = FLOAT_VOLUME_MAX;     
+
+	    mPlayer.setVolume(fVolume, fVolume);
+	}
+	
 }
